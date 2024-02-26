204d5d4b9187f49d53a1aa0b0b9c2f410505d34d
firdevs <firdevsssenturk@gmail.com>
2024-02-26 14:33:15 +0300

    Initial commit

---
+++ src/main/java/com/leetcode/examples/ExamplesApplication.java
@@ -0,0 +1,18 @@
+package com.leetcode.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesApplication.class, args);
+		examples();
+	}
+
+	public static void examples() {
+		System.out.println("fffffffffffffffffffffffffff");
+	}
+
+}
---
+++ src/main/java/com/leetcode/examples/solutions/MergeTwoLists.java
@@ -0,0 +1,31 @@
+package com.leetcode.examples.solutions;
+
+//mergeTwoLists - example 21
+
+/*Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+        Example 2:
+
+        Input: list1 = [], list2 = []
+        Output: []
+        Example 3:
+
+        Input: list1 = [], list2 = [0]
+        Output: [0]*/
+
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            if (list1 == null) return list2;
+            else return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+           return list2;
+        }
+    }
+}
